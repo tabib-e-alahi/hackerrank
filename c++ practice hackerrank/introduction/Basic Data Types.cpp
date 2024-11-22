6b65d45ff13ca3068c3ced27768a374770736d5f
tabib-e-alahi <etabib.alahi@gmail.com>
2024-11-23 00:37:14 +0600

    hackerrank c++ practice 2 problem added

---
+++ c++ practice hackerrank/introduction/Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int, long, char, float, and double, respectively.
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl << b << endl << c << endl;
+    cout << fixed << setprecision(3) << d <<endl;
+    cout << fixed << setprecision(9) << e <<endl;
+    return 0;
+}
+
+
