d8285fee13024c740b5a87a50640e16626c68bf7
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-20 23:27:02 +0600

    10 days of Js: day 1 added

---
+++ 10 Days of Javascript/day_1_arithmetic_operations.js
@@ -0,0 +1,11 @@
+function getArea(length, width) {
+    const area = length * width;
+    
+    return area;
+}
+
+function getPerimeter(length, width) {
+    const perimeter = 2*length + 2 * width;
+
+    return perimeter;
+}
