ff3893daffb722216e611e67b29367e72c8a6028
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-27 23:23:01 +0600

    Assignment 02  Introduction to Algorithms setup done

--- Intro to Algorithms/Assignment 02 _Introduction to Algorithms/Can Go Again.cpp
+++ Intro to Algorithms/Assignment 02 _Introduction to Algorithms/Can Go Again.cpp
@@ -1,7 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Edge{
+    public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+int dis[1005];
+vector<Edge> edge_list;
+int n, e;
 
 int main()
 {
-    
+    cin >> n >> e;
+    while(e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+    }
+
+
 }
