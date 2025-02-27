681a2ee8c5547ecfd2572c5d76de5340d2cefbaf
tabib-e-alahi <etabib.alahi@gmail.com>
2025-02-27 23:42:30 +0600

    Assignment 02  Introduction to Algorithms setup done

--- Intro to Algorithms/Assignment 02 _Introduction to Algorithms/Can Go Again.cpp
+++ Intro to Algorithms/Assignment 02 _Introduction to Algorithms/Can Go Again.cpp
@@ -1,7 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Edge{
-    public:
+class Edge
+{
+public:
     int a, b, w;
     Edge(int a, int b, int w)
     {
@@ -15,24 +16,72 @@ vector<Edge> edge_list;
 int n, e;
 
 void bellman_ford()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
+            int a, b, w;
+            a = ed.a;
+            b = ed.b;
+            w = ed.w;
+
+            if (dis[a] != INT_MAX && dis[a] + w < dis[b])
+                dis[b] = dis[a] + w;
+        }
+    }
+
+    bool cycle = false;
+    for (auto ed : edge_list)
+    {
+        int a, b, w;
+        a = ed.a;
+        b = ed.b;
+        w = ed.w;
+
+        if (dis[a] != INT_MAX && dis[a] + w < dis[b])
+        {
+            cycle = true;
+            break;
+        }
+    }
+
+    if(cycle)
+        cout << "Negative Cycle Detected\n";
+    else{
+        int q;
+        cin >> q;
+        while(q--)
+        {
+            int d;
+            cin >> d;
+            if(dis[d] == INT_MAX){
+                cout << "Not Possible\n";
+            }
+            else{
+                cout << dis[d] << endl;
+            }
+        }
+    }    
+}
 
 int main()
 {
     cin >> n >> e;
-    while(e--)
+    while (e--)
     {
         int a, b, w;
         cin >> a >> b >> w;
         edge_list.push_back(Edge(a, b, w));
     }
-    for(int i = 0; i <= n; i++)
+    for (int i = 0; i <= n; i++)
     {
         dis[i] = INT_MAX;
     }
     int s;
     cin >> s;
     dis[s] = 0;
-    
+
     bellman_ford();
 
     return 0;
